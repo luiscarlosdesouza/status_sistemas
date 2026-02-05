4c6ea50e07e204aa68bfd51932e32d35febb011f
Luis Carlos <luis@DevKiwi.ime.usp.br>
2026-02-05 13:55:28 -0300

    Fix: Atualizado google OAuth para usar Metadata Discovery

--- app.py
+++ app.py
@@ -31,12 +31,7 @@ google = oauth.register(
     name='google',
     client_id=os.getenv('GOOGLE_CLIENT_ID'),
     client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'},
 )
 
